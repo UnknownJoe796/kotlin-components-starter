362fd4390ee0ba6cb45f4c4f0b4cc505b55c52ab
UnknownJoe796 <josephivie@gmail.com>
2015-11-06 16:12:39 -0700

    Bettered TransitionView

---
+++ app/src/main/java/com/ivieleague/kotlin_components_starter/AnotherVC.kt
@@ -0,0 +1,32 @@
+package com.ivieleague.kotlin_components_starter
+
+import android.view.Gravity
+import android.view.View
+import com.lightningkite.kotlincomponents.vertical
+import com.lightningkite.kotlincomponents.viewcontroller.AutocleanViewController
+import com.lightningkite.kotlincomponents.viewcontroller.containers.VCStack
+import com.lightningkite.kotlincomponents.viewcontroller.implementations.VCActivity
+import org.jetbrains.anko._LinearLayout
+import org.jetbrains.anko.button
+import org.jetbrains.anko.onClick
+import org.jetbrains.anko.textView
+
+/**
+ * Created by josep on 11/6/2015.
+ */
+class AnotherVC(val stack: VCStack) : AutocleanViewController() {
+    override fun make(activity: VCActivity): View {
+        return _LinearLayout(activity).apply {
+            setGravity(Gravity.CENTER)
+            orientation = vertical
+
+            textView("Something else")
+
+            button("Go back") {
+                onClick {
+                    stack.pop()
+                }
+            }
+        }
+    }
+}
--- app/src/main/java/com/ivieleague/kotlin_components_starter/StartVC.kt
+++ app/src/main/java/com/ivieleague/kotlin_components_starter/StartVC.kt
@@ -4,8 +4,10 @@ import android.graphics.Bitmap
 import android.view.Gravity
 import android.view.View
 import android.widget.LinearLayout
+import com.lightningkite.kotlincomponents.animation.transitionView
 import com.lightningkite.kotlincomponents.databinding.Bond
 import com.lightningkite.kotlincomponents.image.getImageFromGallery
+import com.lightningkite.kotlincomponents.logging.logD
 import com.lightningkite.kotlincomponents.logging.logE
 import com.lightningkite.kotlincomponents.networking.Networking
 import com.lightningkite.kotlincomponents.ui.inputDialog
@@ -23,6 +25,9 @@ class StartVC(val stack: VCStack) : AutocleanViewController() {
 
     val imageBond: Bond<Bitmap?> = listener(Bond<Bitmap?>(null))
     var image: Bitmap? by imageBond
+
+    val loadingBond: Bond<Boolean> = listener(Bond(false))
+    var loading: Boolean by loadingBond
 
     override fun make(activity: VCActivity): View {
 
@@ -45,24 +50,57 @@ class StartVC(val stack: VCStack) : AutocleanViewController() {
             button("Enter an image URL") {
                 onClick {
                     activity.inputDialog("Enter an image URL", "URL") { url ->
-                        if (url == null) return@inputDialog
-                        Networking.get(url) {
-                            if (it.isSuccessful) {
-                                image = it.bitmap()
-                            } else {
-                                logE(url)
-                                logE(it.code)
-                                logE(it.string())
+                        if (url != null) {
+                            loading = true
+                            Networking.get(url) {
+                                loading = false
+                                if (it.isSuccessful) {
+                                    image = it.bitmap()
+                                } else {
+                                    image = null
+                                    logE(url)
+                                    logE(it.code)
+                                    logE(it.string())
+                                }
                             }
                         }
                     }
                 }
             }
+            transitionView {
 
-            imageView(R.mipmap.ic_launcher) {
+                textView("Loading...") {
+                    setGravity(Gravity.CENTER)
+                }.tag("loading")
+
+                textView("No image.") {
+                    setGravity(Gravity.CENTER)
+                }.tag("none")
+
+                imageView() {
+                    imageBond.bind {
+                        if (it == null) return@bind
+                        imageBitmap = it
+                    }
+                }.tag("image")
+                loadingBond.bind {
+                    if (it) {
+                        animate("loading")
+                    }
+                }
                 imageBond.bind {
-                    if (it == null) return@bind
-                    imageBitmap = it
+                    logD(it)
+                    if (it == null) {
+                        animate("none")
+                    } else {
+                        animate("image")
+                    }
+                }
+            }
+
+            button("I wanna do something else") {
+                onClick {
+                    stack.push(AnotherVC(stack))
                 }
             }
         }
