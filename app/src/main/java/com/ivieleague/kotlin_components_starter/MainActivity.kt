3ab5aaa0c51b30c9042068c1fee1e968c4649d73
UnknownJoe796 <josephivie@gmail.com>
2015-11-06 14:07:26 -0700

    Deleted a distracting file.

--- app/src/main/java/com/ivieleague/kotlin_components_starter/MainActivity.kt
+++ app/src/main/java/com/ivieleague/kotlin_components_starter/MainActivity.kt
@@ -18,7 +18,6 @@ class MainActivity : VCActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
         attach(stack)
     }
 }
