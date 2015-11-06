8acac91f4d5e06aef0619fe3a72c19b603143056
UnknownJoe796 <josephivie@gmail.com>
2015-11-06 15:24:40 -0700

    Added some test code

--- app/src/main/java/com/ivieleague/kotlin_components_starter/StartVC.kt
+++ app/src/main/java/com/ivieleague/kotlin_components_starter/StartVC.kt
@@ -1,23 +1,70 @@
 package com.ivieleague.kotlin_components_starter
 
+import android.graphics.Bitmap
 import android.view.Gravity
 import android.view.View
+import android.widget.LinearLayout
+import com.lightningkite.kotlincomponents.databinding.Bond
+import com.lightningkite.kotlincomponents.image.getImageFromGallery
+import com.lightningkite.kotlincomponents.logging.logE
+import com.lightningkite.kotlincomponents.networking.Networking
+import com.lightningkite.kotlincomponents.ui.inputDialog
+import com.lightningkite.kotlincomponents.vertical
 import com.lightningkite.kotlincomponents.viewcontroller.AutocleanViewController
 import com.lightningkite.kotlincomponents.viewcontroller.containers.VCStack
 import com.lightningkite.kotlincomponents.viewcontroller.implementations.VCActivity
-import org.jetbrains.anko._LinearLayout
-import org.jetbrains.anko.gravity
-import org.jetbrains.anko.textView
+import com.lightningkite.kotlincomponents.viewcontroller.implementations.dialog
+import org.jetbrains.anko.*
 
 /**
  * Created by josep on 11/6/2015.
  */
 class StartVC(val stack: VCStack) : AutocleanViewController() {
+
+    val imageBond: Bond<Bitmap?> = listener(Bond<Bitmap?>(null))
+    var image: Bitmap? by imageBond
+
     override fun make(activity: VCActivity): View {
 
         return _LinearLayout(activity).apply {
-            gravity = Gravity.CENTER
-            textView("Hello World")
+            orientation = LinearLayout.VERTICAL
+            setGravity(Gravity.CENTER)
+
+            textView("Hello World") {
+                setGravity(Gravity.CENTER)
+            }
+
+            button("Get an Image") {
+                onClick {
+                    activity.getImageFromGallery(1000) {
+                        image = it
+                    }
+                }
+            }
+
+            button("Enter an image URL") {
+                onClick {
+                    activity.inputDialog("Enter an image URL", "URL") { url ->
+                        if (url == null) return@inputDialog
+                        Networking.get(url) {
+                            if (it.isSuccessful) {
+                                image = it.bitmap()
+                            } else {
+                                logE(url)
+                                logE(it.code)
+                                logE(it.string())
+                            }
+                        }
+                    }
+                }
+            }
+
+            imageView(R.mipmap.ic_launcher) {
+                imageBond.bind {
+                    if (it == null) return@bind
+                    imageBitmap = it
+                }
+            }
         }
     }
 }
