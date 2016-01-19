58e57f7fcc6f66511a6d81bb16bd9f29ca467cdc
jivie <joseph@lightningkite.com>
2016-01-19 14:11:12 -0700

    Observables added to replace Bonds.

--- app/src/main/java/com/ivieleague/kotlin_components_starter/StartVC.kt
+++ app/src/main/java/com/ivieleague/kotlin_components_starter/StartVC.kt
@@ -5,14 +5,14 @@ import android.view.Gravity
 import android.view.View
 import android.widget.LinearLayout
 import com.lightningkite.kotlincomponents.animation.transitionView
-import com.lightningkite.kotlincomponents.databinding.Bond
 import com.lightningkite.kotlincomponents.image.getImageFromGallery
 import com.lightningkite.kotlincomponents.logging.logD
 import com.lightningkite.kotlincomponents.logging.logE
 import com.lightningkite.kotlincomponents.networking.Networking
+import com.lightningkite.kotlincomponents.observable.Observable
 import com.lightningkite.kotlincomponents.ui.inputDialog
 import com.lightningkite.kotlincomponents.ui.progressButton
-import com.lightningkite.kotlincomponents.viewcontroller.AutocleanViewController
+import com.lightningkite.kotlincomponents.viewcontroller.StandardViewController
 import com.lightningkite.kotlincomponents.viewcontroller.containers.VCStack
 import com.lightningkite.kotlincomponents.viewcontroller.implementations.VCActivity
 import com.lightningkite.kotlincomponents.viewcontroller.linearLayout
@@ -22,116 +22,124 @@ import org.jetbrains.anko.*
  * A ViewController with various tests on it.
  * Created by josep on 11/6/2015.
  */
-class StartVC(val stack: VCStack) : AutocleanViewController() {
+class StartVC(val stack: VCStack) : StandardViewController() {
 
-    val imageBond: Bond<Bitmap?> = listener(object : Bond<Bitmap?>(null) {
+    val imageObs: Observable<Bitmap?> = object : Observable<Bitmap?>(null) {
         override fun set(v: Bitmap?) {
-            this.myValue?.recycle()
+            this.value?.recycle()
             super.set(v)
         }
-    })
-    var image: Bitmap? by imageBond
+    }
+    var image: Bitmap? by imageObs
 
-    val loadingBond: Bond<Boolean> = listener(Bond(false))
-    var loading: Boolean by loadingBond
+    val loadingObs: Observable<Boolean> = Observable(false)
+    var loading: Boolean by loadingObs
 
-    override fun make(activity: VCActivity): View {
+    val textObs = Observable("Start Text")
+    var text by textObs
 
-        return linearLayout(activity) {
-            orientation = LinearLayout.VERTICAL
-            setGravity(Gravity.CENTER)
+    override fun makeView(activity: VCActivity): View = linearLayout(activity) {
+        orientation = LinearLayout.VERTICAL
+        setGravity(Gravity.CENTER)
 
-            textView("Hello World") {
-                gravity = Gravity.CENTER
-            }
+        textView("Hello World") {
+            gravity = Gravity.CENTER
+        }
 
-            button("Get an Image") {
-                onClick {
-                    activity.getImageFromGallery(1000) {
-                        image = it
-                    }
+        editText() {
+            bindString(textObs)
+        }
+
+        textView() {
+            bindString(textObs)
+        }
+
+        button("Get an Image") {
+            onClick {
+                activity.getImageFromGallery(1000) {
+                    image = it
                 }
             }
+        }
 
-            progressButton("Enter an image URL") {
-                onClick {
-                    activity.inputDialog("Enter an image URL", "URL") { url ->
-                        if (url != null) {
-                            running = true
-                            loading = true
-                            Networking.get(url) {
-                                running = false
-                                loading = false
-                                if (it.isSuccessful) {
-                                    image = it.bitmap()
-                                } else {
-                                    image = null
-                                    logE(url)
-                                    logE(it.code)
-                                    logE(it.string())
-                                }
+        progressButton("Enter an image URL") {
+            onClick {
+                activity.inputDialog("Enter an image URL", "URL") { url ->
+                    if (url != null) {
+                        running = true
+                        loading = true
+                        Networking.get(url) {
+                            running = false
+                            loading = false
+                            if (it.isSuccessful) {
+                                image = it.bitmap()
+                            } else {
+                                image = null
+                                logE(url)
+                                logE(it.code)
+                                logE(it.string())
                             }
                         }
                     }
                 }
             }
+        }
 
-            progressButton("Lorem Pixel") {
-                onClick {
-                    running = true
-                    loading = true
-                    Networking.get("http://lorempixel.com/300/300") {
-                        running = false
-                        loading = false
-                        if (it.isSuccessful) {
-                            image = it.bitmap()
-                        } else {
-                            image = null
-                        }
-                    }
-                }
-            }
-            transitionView {
-
-                textView("Loading...") {
-                    gravity = Gravity.CENTER
-                }.tag("loading")
-
-                textView("No image.") {
-                    gravity = Gravity.CENTER
-                }.tag("none")
-
-                imageView() {
-                    imageBond.bind {
-                        if (it == null) return@bind
-                        imageBitmap = it
-                    }
-                }.tag("image")
-                loadingBond.bind {
-                    if (it) {
-                        animate("loading")
-                    }
-                }
-                imageBond.bind {
-                    logD(it)
-                    if (it == null) {
-                        animate("none")
+        progressButton("Lorem Pixel") {
+            onClick {
+                running = true
+                loading = true
+                Networking.get("http://lorempixel.com/300/300") {
+                    running = false
+                    loading = false
+                    if (it.isSuccessful) {
+                        image = it.bitmap()
                     } else {
-                        animate("image")
+                        image = null
                     }
                 }
             }
+        }
+        transitionView {
 
-            button("Test Socket IO") {
-                onClick {
-                    stack.push(SocketIOVC(stack))
+            textView("Loading...") {
+                gravity = Gravity.CENTER
+            }.tag("loading")
+
+            textView("No image.") {
+                gravity = Gravity.CENTER
+            }.tag("none")
+
+            imageView() {
+                connect(imageObs) {
+                    if (it == null) return@connect
+                    imageBitmap = it
+                }
+            }.tag("image")
+            connect(loadingObs) {
+                if (it) {
+                    animate("loading")
                 }
             }
-
-            button("I wanna do something else") {
-                onClick {
-                    stack.push(AnotherVC(stack))
+            connect(imageObs) {
+                logD(it)
+                if (it == null) {
+                    animate("none")
+                } else {
+                    animate("image")
                 }
+            }
+        }
+
+        button("Test Socket IO") {
+            onClick {
+                stack.push(SocketIOVC(stack))
+            }
+        }
+
+        button("I wanna do something else") {
+            onClick {
+                stack.push(AnotherVC(stack))
             }
         }
     }
