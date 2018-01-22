5e45064beddec9f2c966df4ee144b34b75d44d3c
Joseph Ivie <josephivie@gmail.com>
2018-01-22 11:45:56 -0700

    Deprecating updates

--- app/src/main/java/com/ivieleague/kotlin_components_starter/MainVC.kt
+++ app/src/main/java/com/ivieleague/kotlin_components_starter/MainVC.kt
@@ -2,15 +2,12 @@ package com.ivieleague.kotlin_components_starter
 
 import android.view.Gravity
 import android.view.View
-import com.lightningkite.kotlin.anko.animation.swapView
 import com.lightningkite.kotlin.anko.elevationCompat
 import com.lightningkite.kotlin.anko.lifecycle
-import com.lightningkite.kotlin.anko.observable.CentralRenderMappings
-import com.lightningkite.kotlin.anko.observable.bindRenderMap
 import com.lightningkite.kotlin.anko.viewcontrollers.AnkoViewController
 import com.lightningkite.kotlin.anko.viewcontrollers.VCContext
 import com.lightningkite.kotlin.anko.viewcontrollers.containers.VCStack
-import com.lightningkite.kotlin.observable.property.bind
+import com.lightningkite.kotlin.lifecycle.bind
 import org.jetbrains.anko.*
 import org.jetbrains.anko.appcompat.v7.actionMenuView
 import org.jetbrains.anko.appcompat.v7.toolbar
@@ -36,7 +33,7 @@ class MainVC() : AnkoViewController() {
             actionMenuView {
             }.lparams(Gravity.RIGHT)
 
-            lifecycle.bind(stack) {
+            lifecycle.bind(stack.onSwap, stack.current) {
                 this.title = it.getTitle(resources)
                 setNavigationOnClickListener { stack.pop() }
                 if (stack.size > 1) {
@@ -49,7 +46,7 @@ class MainVC() : AnkoViewController() {
         }.lparams(matchParent, wrapContent)
 
         coordinatorLayout {
-            this.swapView { bindRenderMap(ui.owner, stack, CentralRenderMappings) }.lparams(matchParent, matchParent)
+            viewContainer(ui.owner, stack).lparams(matchParent, matchParent)
         }.lparams(matchParent, 0, 1f)
     }
 
